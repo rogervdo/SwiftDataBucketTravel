d7952972e90e872f081d13bb7f6b2531ead1c934
rogervdo <rogervdo@icloud.com>
2025-10-20 13:47:41 -0600

    Initial Commit

---
+++ SwiftDataBucketTravel/SwiftDataBucketTravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataBucketTravelApp.swift
+//  SwiftDataBucketTravel
+//
+//  Created by Rogelio Villarreal on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataBucketTravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
